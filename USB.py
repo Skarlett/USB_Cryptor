1515e9ee7069bd11368b95ec63047fa3f7beb248
Skarlett <Skarlett@users.noreply.github.com>
2017-07-03 04:25:48 -0500

    0.0.2
    Added a representation of the object.

--- USB.py
+++ USB.py
@@ -27,5 +27,9 @@ class USB:
       for i, x in enumerate(sys("findmnt -%s \"%s\"" % (v, d)).split('\n')):
         if i > 0:
           return namedtuple('USB_info', 'target source fstype options')(*splitchar.join(x.split()).split(splitchar, 3))
-
+  
+  
+  def __repr__(self):
+    return '<%s | %s>' % (self.data.source, self.data.target)
+  
 # print USB('/dev/sdb1').data
