967a39bfa280a3f9137b0873df2eab7888238f55
Skarlett <Skarlett@users.noreply.github.com>
2017-07-04 09:15:53 -0500

    0.0.3
    Now requires a little more user interaction.

--- Sync/install.sh
+++ Sync/install.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
-apt-get -y install python-dev python-pip
+apt-get -y install python-dev python-pip libnotify-cil-dev
 python -m pip install notify2
 
+
 chmod +x cronjob.py
-mkdir /etc/backup_rsync/
 
-cp cronjob.py /etc/backup_rsync/
-cp USB.py /etc/backup_rsync/
+mkdir ~/.rsync_backup/
+mkdir ~/.rsync_backup/cron
 
-echo "Running First run"
-python /etc/backup_rsync/cronjob.py
+cp cronjob.py ~/.rsync_backup/
+cp cronjob.py ~/.rsync_backup/cron
+
+cp USB.py ~/.rsync_backup/
+cp USB.py ~/.rsync_backup/cron
+
+touch ~/.rsync_backup/.installdirectory
+touch ~/.rsync_backup/log.log
+touch ~/.rsync_backup/cron/log.log
+
+chown -R $USER ~/.rsync_backup
+chmod -R 0777 ~/.rsync_backup
 echo "setting up cronjob in hourly"
+echo ""
+echo ""
 
-su -c "echo \"59 * * * * export DISPLAY=:0.0 && export XAUTHORITY=/home/$USER/.Xauthority && sudo -u $USER /usr/bin/python /etc/backup_rsync/cronjob.py\" >> /var/spool/cron/crontabs/root"
-
-echo "done. goodbye."
+echo "run the following"
+echo ""
+echo 'su -c "echo \"0 * * * * export DISPLAY=:0.0 && export XAUTHORITY=/home/$USER/.Xauthority && sudo -u $USER /usr/bin/python /home/$USER/.rsync_backup/cron/cronjob.py\" >> /var/spool/cron/crontabs/$USER"'
+echo ""
+echo "Then run."
+echo "python ~/.rsync_backup/cronjob.py"
