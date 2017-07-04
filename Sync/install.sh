7d082c84cc16cd5f3200e249db3ac21bec48601e
Skarlett <Skarlett@users.noreply.github.com>
2017-07-04 05:33:01 -0500

    0.0.2
    Added for updated cronjob.py - Now implements notify-send through cronjob - Also install python requirements for install libraries and a library from python.

--- Sync/install.sh
+++ Sync/install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+apt-get -y install python-dev python-pip
+python -m pip install notify2
+
 chmod +x cronjob.py
 mkdir /etc/backup_rsync/
+
 cp cronjob.py /etc/backup_rsync/
 cp USB.py /etc/backup_rsync/
+
+echo "Running First run"
 python /etc/backup_rsync/cronjob.py
 echo "setting up cronjob in hourly"
-su -c "echo \"@hourly /etc/backup_rsync/cronjob.py\" >> /var/spool/cron/crontabs/root"
+
+su -c "echo \"59 * * * * export DISPLAY=:0.0 && export XAUTHORITY=/home/$USER/.Xauthority && sudo -u $USER /usr/bin/python /etc/backup_rsync/cronjob.py\" >> /var/spool/cron/crontabs/root"
+
 echo "done. goodbye."
